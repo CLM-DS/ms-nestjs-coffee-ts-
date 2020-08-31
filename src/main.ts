742a15b83e055b902cd73a7f11ad7fcb4bc48f0b
Sergio Pino <ext_sapinol@falabella.cl>
2020-08-31 12:18:34 -0400

    base to database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, // validate the contract vs entity
+      // transform: true, // 👈 force to transform data to our type
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
