19cf39932066fd4deefe3b8a0ef3beb40def544a
Leafar Maina <lmaina@clmconsultores.com>
2021-06-07 10:24:01 -0400

    fix: needs on pipeline jobs feat: more checks for loadtesting

--- test/coffee-api.loadtest.js
+++ test/coffee-api.loadtest.js
@@ -1,9 +1,12 @@
 import http from "k6/http";
 import { check } from "k6";
+import { Rate } from "k6/metrics";
 
 // These are still very much WIP and untested, but you can use them as is or write your own!
 import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
+// A custom metric to track failure rates
+var failureRate = new Rate("check_failure_rate");
 
 export let options = {
     setupTimeout: '100s',
@@ -20,12 +23,19 @@ export let options = {
     thresholds: {
         // Declare a threshold over all HTTP response times,
         // the 95th percentile should not cross 500ms
-        http_req_duration: ["p(95)<1000"],
+        "http_req_duration": ["p(95)<1000"],
 
         // Declare a threshold over HTTP response times for all data points
         // where the URL tag is equal to "http://httpbin.org/post",
         // the max should not cross 1000ms
         "http_req_duration{url:https://apicoffee.clm.team/coffees}": ["max<1000"],
+        // Thresholds based on the custom metric we defined and use to track application failures
+        "check_failure_rate": [
+            // Global failure rate should be less than 1%
+            "rate<0.01",
+            // Abort the test early if it climbs over 5%
+            { threshold: "rate<=0.05", abortOnFail: true },
+        ],
     }
 };
 
@@ -33,10 +43,12 @@ export default function() {
     let res = http.get("https://apicoffee.clm.team/coffees");
     // Use JSON.parse to deserialize the JSON (instead of using the r.json() method)
     let j = JSON.parse(res.body);
-    // Verify response
-    check(res, {
+    // check() returns false if any of the specified conditions fail
+    let checkRes = check(res, {
         "status is 200": (r) => r.status === 200,
     });
+    // We reverse the check() result since we want to count the failures
+    failureRate.add(!checkRes);
 }
 
 export function handleSummary(data) {
